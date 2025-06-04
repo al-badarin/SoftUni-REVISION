973eeec0fd0a4a692884adf9235ad27ac842d037
al-badarin <jamalalbadarin@gmail.com>
2025-06-04 14:50:00 +0300

    Create 4_convertMetersToKilometers.js

---
+++ Data Types and Variables/4_convertMetersToKilometers.js
@@ -0,0 +1,7 @@
+function convertMetersToKilometres(distanceInMetres) {
+  let distanceInKilometres = distanceInMetres / 1000;
+  console.log(distanceInKilometres.toFixed(2));
+}
+
+convertMetersToKilometres(1852);
+convertMetersToKilometres(798);
