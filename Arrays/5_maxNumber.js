31efb62d794c29136bb1ebf9d32a086410be72ab
al-badarin <jamalalbadarin@gmail.com>
2025-06-08 00:50:05 +0300

    Update 5_maxNumber.js

--- Arrays/5_maxNumber.js
+++ Arrays/5_maxNumber.js
@@ -1,3 +1,35 @@
-function solve(){
-    
+function solve(arr) {
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let isLargest = true;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] <= arr[j]) {
+        isLargest = false;
+        break;
+      }
+    }
+
+    if (isLargest) {
+      resultArr.push(arr[i]);
+    }
+  }
+
+  console.log(resultArr.join(' '));
+
+  // *manual print
+  //   let result = '';
+  //   for (let i = 0; i < resultArr.length; i++) {
+  //     result += resultArr[i];
+  //     if (i < resultArr.length - 1) {
+  //       result += ' ';
+  //     }
+  //   }
+
+  //   console.log(result);
 }
+
+solve([1, 4, 3, 2]);
+solve([14, 24, 3, 19, 15, 17]);
+solve([41, 41, 34, 20]);
