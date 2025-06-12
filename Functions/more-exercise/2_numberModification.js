cd63f520119aac588cdb05a9dff174b71fdabd56
al-badarin <jamalalbadarin@gmail.com>
2025-06-12 22:53:03 +0300

    Create 2_numberModification.js

---
+++ Functions/more-exercise/2_numberModification.js
@@ -0,0 +1,22 @@
+function numberMofification(num) {
+  let numString = num.toString();
+  let sum = 0;
+
+  for (let digit of numString) {
+    sum += Number(digit);
+  }
+
+  let average = sum / numString.length;
+
+  while (average <= 5) {
+    numString += '9';
+    sum += 9;
+    average = sum / numString.length;
+  }
+
+  console.log(numString);
+}
+
+// Example usage:
+numberMofification(101); // Output: 1019
+// numberMofification(5835); // Output: 5835
