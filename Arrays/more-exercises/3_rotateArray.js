a39b0316d3462faf533e0cc72765caea00fee2dc
al-badarin <jamalalbadarin@gmail.com>
2025-06-12 22:08:32 +0300

    Update 3_rotateArray.js

--- Arrays/more-exercises/3_rotateArray.js
+++ Arrays/more-exercises/3_rotateArray.js
@@ -1,4 +1,12 @@
-function rotateArray(arr){
-    let rotationSteps = Number(arr.pop())
-    
+function rotateArray(arr) {
+  let rotationSteps = Number(arr.pop());
+
+  for (let i = 0; i < rotationSteps % arr.length; i++) {
+    let lastElement = arr.pop();
+    arr.unshift(lastElement);
+  }
+  
+  return arr.join(' ');
 }
+
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
