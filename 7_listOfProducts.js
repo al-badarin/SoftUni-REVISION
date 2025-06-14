a0c518c06ad038378dd86e08cc2809f82298a688
al-badarin <jamalalbadarin@gmail.com>
2025-06-14 18:50:01 +0300

    Update 7_listOfProducts.js

--- 7_listOfProducts.js
+++ 7_listOfProducts.js
@@ -1,9 +1,8 @@
 function listOfProducts(arr) {
+  let sorted = arr.sort((a, b) => a.localeCompare(b));
 
-    let sorted = arr.sort();
-
-    for (let i = 0; i < sorted.length; i++) {
-        console.log(`${i + 1}.${sorted[i]}`);
-    }
+  for (let i = 0; i < sorted.length; i++) {
+    console.log(`${i + 1}.${sorted[i]}`);
+  }
 }
 listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
