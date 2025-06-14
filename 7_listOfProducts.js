549f1ed3294e541b78ae3026b17bc81c2c82240b
al-badarin <jamalalbadarin@gmail.com>
2025-06-14 18:51:13 +0300

    Delete 7_listOfProducts.js

--- 7_listOfProducts.js
+++
@@ -1,8 +0,0 @@
-function listOfProducts(arr) {
-  let sorted = arr.sort((a, b) => a.localeCompare(b));
-
-  for (let i = 0; i < sorted.length; i++) {
-    console.log(`${i + 1}.${sorted[i]}`);
-  }
-}
-listOfProducts(['Potatoes', 'Tomatoes', 'Onions', 'Apples']);
