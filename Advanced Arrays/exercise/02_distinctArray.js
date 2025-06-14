d2f9721f4b522011ffa1c819ffea01965827579d
al-badarin <jamalalbadarin@gmail.com>
2025-06-14 20:11:40 +0300

    Create 02_distinctArray.js

---
+++ Advanced Arrays/exercise/02_distinctArray.js
@@ -0,0 +1,11 @@
+function distinctArray(arr) {
+  let result = [];
+
+  for (let el of distinctArray) {
+    if (!result.includes(el)) {
+      result.push(el);
+    }
+  }
+
+  console.log(result.join(' '));
+}
