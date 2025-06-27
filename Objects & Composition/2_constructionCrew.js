27ecf289d35057bd9a87e177e7afa635f84bac17
al-badarin <jamalalbadarin@gmail.com>
2025-06-27 21:46:00 +0300

    Create 2_constructionCrew.js

---
+++ Objects & Composition/2_constructionCrew.js
@@ -0,0 +1,18 @@
+function constructionCrew(worker) {
+  if (worker.dizziness) {
+    const requiredWater = 0.1 * worker.weight * worker.experience;
+    worker.levelOfHydrated += requiredWater;
+    worker.dizziness = false;
+  }
+
+  return worker;
+}
+
+const worker = {
+  weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true,
+};
+
+console.log(constructionCrew(worker));
