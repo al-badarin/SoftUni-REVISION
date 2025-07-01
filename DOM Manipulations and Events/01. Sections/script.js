99c7b73345bc6bcdbbe9641eeeda896115101137
al-badarin <jamalalbadarin@gmail.com>
2025-07-01 10:32:04 +0300

    solved 1. Sections

--- DOM Manipulations and Events/01. Sections/script.js
+++ DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+  let divParentElement = document.getElementById('content');
+  let elements = words;
+
+  for (let i = 0; i < elements.length; i++) {
+    let div = document.createElement('div');
+    let p = document.createElement('p');
+    let text = document.createTextNode(elements[i]);
+
+    p.appendChild(text);
+    p.style.display = 'none';
+    div.appendChild(p);
+    div.addEventListener('click', function showParagraphs(e) {
+      e.target.children[0].style.display = 'inline';
+    });
+
+    divParentElement.appendChild(div);
+  }
 }
