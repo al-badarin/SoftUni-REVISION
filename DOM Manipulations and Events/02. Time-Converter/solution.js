c18aef0ae3af19502021115c8ea3f8b245285d0b
al-badarin <jamalalbadarin@gmail.com>
2025-07-01 20:29:26 +0300

    solved 2. Time-Converter

--- DOM Manipulations and Events/02. Time-Converter/solution.js
+++ DOM Manipulations and Events/02. Time-Converter/solution.js
@@ -1,4 +1,43 @@
 function attachEventsListeners() {
+  let daysInputElement = document.getElementById('days');
+  let hoursInputElement = document.getElementById('hours');
+  let minutesInputElement = document.getElementById('minutes');
+  let secondsInputElement = document.getElementById('seconds');
 
-    console.log('TODO:...');
+  const rations = {
+    days: 1,
+    hours: 24,
+    minutes: 1440,
+    seconds: 86400,
+  };
+
+  document.getElementById('daysBtn').addEventListener('click', convertHandler);
+  document.getElementById('hoursBtn').addEventListener('click', convertHandler);
+  document
+    .getElementById('minutesBtn')
+    .addEventListener('click', convertHandler);
+  document
+    .getElementById('secondsBtn')
+    .addEventListener('click', convertHandler);
+
+  function convert(value, unit) {
+    let days = value / rations[unit];
+
+    return {
+      days: days,
+      hours: days * rations.hours,
+      minutes: days * rations.minutes,
+      seconds: days * rations.seconds,
+    };
+  }
+
+  function convertHandler(e) {
+    let input = e.target.parentElement.querySelector('input[type="text"]');
+
+    let time = convert(Number(input.value), input.id);
+    days.value = time.days;
+    hours.value = time.hours;
+    minutes.value = time.minutes;
+    seconds.value = time.seconds;
+  }
 }
