6ee63faf6e843907804e3ebfdf07e590a1060f7b
al-badarin <jamalalbadarin@gmail.com>
2025-07-07 12:34:05 +0300

    Implement dynamic email validation logic
    Added an event listener to validate the email input on change. The input is checked against a regex pattern, and the 'error' class is toggled based on validity.

--- Unit Testing/06.Dynamic-Validation/app.js
+++ Unit Testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+  document.getElementById('email').addEventListener('change', onChange);
+
+  function onChange(e) {
+    const pattern = /[a-z]+@[a-z]+\.[a-z]+/gm;
+
+    if (pattern.test(e.target.value)) {
+      e.target.classList.remove('error');
+    } else {
+      e.target.classList.add('error');
+    }
+  }
 }
